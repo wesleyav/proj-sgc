d18a2ddf0854a4514b39d46e7443a2d8abd7b39a
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-07-22 19:17:15 -0300

    Estrutura do projeto

---
+++ src/test/java/com/wesleyav/sgc/SgcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleyav.sgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
